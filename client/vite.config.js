8e38fbe18dff586af74e3f027f4ac7842aea3ef9
emma reed <emmakatharynreed@icloud.com>
2025-03-15 16:07:02 -0600

    Update Vite configuration to set base path for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/<repository-name>/", // Replace <repository-name> with your GitHub repository name
+  base: "/book-club/", // Replace <repository-name> with your GitHub repository name
   plugins: [react()],
 });
